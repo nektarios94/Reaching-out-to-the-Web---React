1b4177ba6009713f67de3d720809fab7cb409f6e
Nektarios Ntontoulos <nekis.dodos@gmail.com>
2021-02-24 12:55:47 +0200

    update

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -40,7 +40,6 @@ class Posts extends Component {
     }
     
     render() {
-        console.log('params.id: ', this.props.match.params.id);
         let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
         if (!this.state.error) {
             posts = this.state.posts.map( post => {
